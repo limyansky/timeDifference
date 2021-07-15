3a05b00f5a4542a751685222ac57185f10fef971
Limyansky <brent.limyansky@ucsc.edu>
2021-07-15 16:06:14 -0700

    Accept command line arguments

--- visualizeResults.py
+++ visualizeResults.py
@@ -3,9 +3,30 @@
 # Handles commandline inputs
 import argparse
 
+# Handles csv files
+import pandas as pd
+
 
 # The function that will run when the script is called
 def main():
+    """
+    Takes .csv output file and produces summary output for further analysis.
+
+    Parameters:
+        csv_file: The output of a blind search
+
+    """
+
+    # Create the argument parser and add the docstring
+    parser = argparse.ArgumentParser(description=__doc__)
+
+    # Add arguments
+    parser.add_argument('csv_file',
+                        help='The output .csv file of a blind search')
+
+    # Extract the arguments from the parser
+    args = parser.parse_args()
+
 
     return 0
 
